602cbdb7ed6bd2d1f7f3abc2d82dff78de1cea6d
folajimi alt mac <folajimialtmac@folajimi-Alt-MacBook-Pro.local>
2024-01-02 17:19:23 +0000

    updated code to use chat model and prompt templates

--- utils/makechain.py
+++ utils/makechain.py
@@ -1,8 +1,15 @@
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import Pinecone
 from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
-CONDENSE_PROMPT = """
+
+condense_question_template = """
 Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
@@ -10,33 +17,42 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:
 """
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question_template)
 
-# change to your own 'system' prompt
-QA_PROMPT = """
+system_template = """
 You are an AI assistant providing helpful advice. Use the following pieces of context to answer the question at the end.
 If you don't know the answer based on the context below, just say "Hmm, I'm not sure." DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
+----------------
 {context}
 
-Question: {question}
 Helpful answer in markdown:
 """
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+QA_PROMPT = ChatPromptTemplate.from_messages(messages)
 
 def make_chain(vectorstore: Pinecone, openai_api_key: str):
-    model = OpenAI(
-        temperature=0,  # increase temperature to get more creative answers
+    # model = OpenAI(
+    #     temperature=0.3,  # increase temperature to get more creative answers
+    #     model_name='gpt-3.5-turbo',
+    #     openai_api_key=openai_api_key,  # change this to gpt-4 if you have access to the API
+    # )
+    model = ChatOpenAI(
+        temperature=0.3,  # increase temperature to get more creative answers
         model_name='gpt-3.5-turbo',
         openai_api_key=openai_api_key,  # change this to gpt-4 if you have access to the API
     )
 
     chain = ConversationalRetrievalChain.from_llm(
-        model,
-        vectorstore.as_retriever(),
-        condense_question_prompt=CONDENSE_PROMPT,
+        llm=model,
+        retriever=vectorstore.as_retriever(),
+        condense_question_prompt=CONDENSE_QUESTION_PROMPT,
+        return_source_documents=True,
         combine_docs_chain_kwargs={
-            'qaTemplate': QA_PROMPT,
-            'returnSourceDocuments': True
+            'prompt': QA_PROMPT,
         }
     )
 
